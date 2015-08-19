95d25a28867c6e834cd01eeee0a1d83d0d19b38b
Paul Elwood <paul.elwood@brother.com>
2015-08-18 20:49:26 -0400

    change to footer
    added studios in footer link

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <?php
 echo "<div class='footer'>
-<a href='contact.php'>Contact MultiGrain</a><a href='about.php'>About Us</a><a href='#'>Why Whiteboard Videos Work So Well:</a>
+<a href='contact.php'>Contact MultiGrain Studios</a><a href='about.php'>About Us</a><a href='#'>Why Whiteboard Videos Work So Well:</a>
 </div>";
 ?>
